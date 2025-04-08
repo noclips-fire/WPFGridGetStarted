b18629b5f6bc3c066bd0b73fd1f88eef9c58e4d9
noclips-fire <shevelovdmytro2006@gmail.com>
2025-04-08 12:02:03 +0200

    Add project files.

---
+++ TestProjekt2/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.XtraPrinting.Native;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Windows.Controls;
+
+
+namespace TestProjekt2
+{
+    public class ViewModel : ViewModelBase
+    {
+        NorthwindEntities northwindDbContext;
+
+        public ViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Orders = new ObservableCollection<Order>();
+                Shippers = new ObservableCollection<Shipper>();
+                Employees = new ObservableCollection<Employee>();
+
+            }
+            else
+            {
+                northwindDbContext = new NorthwindEntities();
+
+                northwindDbContext.Orders.Load();
+                Orders = northwindDbContext.Orders.Local;
+
+                northwindDbContext.Shippers.Load();
+                Shippers = northwindDbContext.Shippers.Local;
+
+                northwindDbContext.Employees.Load();
+                Employees = northwindDbContext.Employees.Local;
+            }
+        }
+        public ObservableCollection<Order> Orders
+        {
+            get => GetValue<ObservableCollection<Order>>();
+            private set => SetValue(value);
+        }
+
+        public ObservableCollection<Shipper> Shippers
+        {
+            get => GetValue<ObservableCollection<Shipper>>();
+            private set => SetValue(value);
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => GetValue<ObservableCollection<Employee>>();
+            private set => SetValue(value);
+        }
+
+    }
+}
